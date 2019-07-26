96e42d921f6da3a1fdd3da27e861e345aefe124d
Rebecca Lelew <rebecca.lelew@springernature.com>
2019-07-26 13:00:44 +0100

    Create initial tick function

--- src/main/scala/Simulator.scala
+++ src/main/scala/Simulator.scala
@@ -1,33 +1,42 @@
-class Simulator(val lifts: Int, val floors: Int, randomiser: Randomiser) {
-  private def randomDestination(location: Int): Int = {
-    val viableFloors = (0 to this.floors).toSet diff Set(location)
-    viableFloors.toVector(randomiser.nextInt(viableFloors.size))
-  }
+class Simulator(val lifts: Int, val floors: Int, randomiser: Randomiser = scalaRandomizer) {
+  def initialTick(): Map[String, List[ElevatorObject]] =
+    Map("lifts" -> List.fill(lifts)(Lift(0, None, List())), "people" -> List())
 
   def generatePeople(existingPeople: List[Person]): List[Person] = {
     val floors = this.floors
     val people = for (floor <- 0 to floors) yield {
-      val noOfPeople = numberToPersonMappings(randomiser.nextInt(100))
-      List.fill(noOfPeople)(Person(start = floor, destination = randomDestination(floor)))
+      val noOfPeople = randomiser.randomNumberOfPeople()
+      List.fill(noOfPeople)(Person(start = floor, destination = randomiser.randomDestination(floor, this.floors)))
     }
     people.toList.flatten
   }
-
-    private def numberToPersonMappings(n: Int): Int = {
-      n match {
-        case x if x < 35 => 0
-        case x if x < 55 => 1
-        case x if x < 75 => 2
-        case x if x < 90 => 3
-        case x if x < 97 => 4
-        case _ => 5
-      }
-    }
 }
 
 trait Randomiser{
-  def nextInt(i: Int): Int
+  def randomDestination(location: Int, floors: Int): Int
+  def randomNumberOfPeople(): Int
 }
 
+trait ElevatorObject
 
-case class Person(start: Int, destination: Int)
+case class Lift(location: Int, destination: Option[Int], people: List[Person]) extends ElevatorObject
+
+case class Person(start: Int, destination: Int) extends ElevatorObject
+
+object scalaRandomizer extends Randomiser {
+  val r = scala.util.Random
+  def randomDestination(location: Int, floors: Int): Int = {
+    val viableFloors = (0 to floors).toSet diff Set(location)
+    viableFloors.toVector(r.nextInt(viableFloors.size))
+  }
+  def randomNumberOfPeople(): Int =
+    r.nextInt(100) match {
+      case x if x < 35 => 0
+      case x if x < 55 => 1
+      case x if x < 75 => 2
+      case x if x < 90 => 3
+      case x if x < 97 => 4
+      case _ => 5
+    }
+}
+
--- src/test/scala/SimulatorSpec.scala
+++ src/test/scala/SimulatorSpec.scala
@@ -3,7 +3,8 @@ import org.scalatest._
 class SimulatorSpec extends FlatSpec with Matchers {
 
   object mockRandomiser extends Randomiser {
-    override def nextInt(i: Int): Int = i / 2
+    override def randomDestination(location: Int, floors: Int): Int = 2
+    override def randomNumberOfPeople(): Int = 1
   }
   "A simulator" should "be generated with a number of floors and lifts" in {
 
@@ -12,11 +13,19 @@ class SimulatorSpec extends FlatSpec with Matchers {
     s.lifts shouldBe 2
   }
 
+  "A simulator" should "be start with lifts on the ground floor with no destination or people, and no people waiting" in {
+
+    val s = new Simulator(floors = 5, lifts = 2, randomiser = mockRandomiser)
+    val state = s.initialTick()
+    state("lifts") shouldBe List(Lift(0, None, List()), Lift(0, None, List()))
+    state("people") shouldBe List()
+  }
+
   it should "generate new people with a destination and a location at a new tick" in {
     val s = new Simulator(floors = 5, lifts = 2, randomiser = mockRandomiser)
-    val p = s.generatePeople(List())
-    assert(p.size == 6)
-    assert(p(0).start == 0)
-    assert(p(0).destination == 2)
+    val people = s.generatePeople(List())
+    assert(people.size == 6)
+    assert(people(0).start == 0)
+    assert(people(0).destination == 2)
   }
 }
