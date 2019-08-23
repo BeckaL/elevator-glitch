cc2f69f970e5c27fffc1e132180988192372ccaa
Rebecca Lelew <rebecca.lelew@springernature.com>
2019-08-23 11:52:21 +0100

    Create CompletePrintedJourney trait for tests

--- src/test/scala/IntegrationSpec.scala
+++ src/test/scala/IntegrationSpec.scala
@@ -1,44 +1,52 @@
 import org.scalatest.{FlatSpec, Matchers}
 
-class IntegrationSpec extends FlatSpec with Matchers {
+class IntegrationSpec extends FlatSpec with Matchers with CompletePrintedJourney {
 
   "A simulator" should "convert an elevator state to a state to render" in {
-    val state = ElevatorState(List(Person(0, 0, 0)), List(Lift(0, None, List(), "")), List(), 0, List())
-
+    val state = ElevatorState(List(Person(1, 2, 0)), List(Lift(0, None, List(), "")), List(), 0, List())
     val r = SceneRenderer
+
     val sceneToPrint = r.convertStateToScene(state)
-    sceneToPrint shouldBe SceneToRender(LiftToRender(0, 0, ""), List(PersonOnFloor(0)), List())
-    val printedScene = r.createScene(sceneToPrint.copy(peopleWaiting = List(PersonOnFloor(1))))
-    printedScene shouldBe partialSTate
+    sceneToPrint shouldBe SceneToRender(LiftToRender(0, 0, ""), List(PersonOnFloor(1)), List())
+
+    val printedScene = r.createScene(sceneToPrint)
+    printedScene shouldBe stateOne
   }
 
-  val partialSTate = List(
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "─────────────────          ───── ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "─────────────────          ───── ",
-    "                           ",
-    "                           ",
-    "                  ╔══════╗ ",
-    "                  ║      ║ ",
-    "                  ║      ║ ",
-    "                  ║      ║ ",
-    "                  ║      ║ ",
-    "              웃  ║      ║ ",
-    "───────────────── ╚══════╝ ───── "
+  "A simulator" should "convert a second elevator state to a state to render" in {
+    val state = ElevatorState(List(Person(1, 2, 0)), List(Lift(0, None, List(), "left")), List(), 0, List())
+    val r = SceneRenderer
 
-  )
+    val sceneToPrint = r.convertStateToScene(state)
+    val printedScene = r.createScene(sceneToPrint)
+    printedScene shouldBe stateTwo
+  }
+
+  "A simulator" should "convert a third elevator state to a state to render" in {
+    val state = ElevatorState(List(), List(Lift(0, None, List(Person(1, 2, 0)), "left")), List(), 0, List())
+    val r = SceneRenderer
+
+    val sceneToPrint = r.convertStateToScene(state)
+    val printedScene = r.createScene(sceneToPrint)
+    printedScene shouldBe stateThree
+  }
+
+
+  "A simulator" should "convert a fourth elevator state to a state to render" in {
+    val state = ElevatorState(List(), List(Lift(0, None, List(Person(1, 2, 0)), "")), List(), 0, List())
+    val r = SceneRenderer
+
+    val sceneToPrint = r.convertStateToScene(state)
+    val printedScene = r.createScene(sceneToPrint)
+    printedScene shouldBe stateFour
+  }
+
+  "A simulator" should "convert a fifth elevator state to a state to render" in {
+    val state = ElevatorState(List(), List(Lift(1, None, List(Person(1, 2, 0)), "")), List(), 0, List())
+    val r = SceneRenderer
+
+    val sceneToPrint = r.convertStateToScene(state)
+    val printedScene = r.createScene(sceneToPrint)
+    printedScene shouldBe stateFive
+  }
 }
