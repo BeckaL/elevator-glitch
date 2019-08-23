45c57bbe029ba296cea23594007c5cd7049aaef9
Rebecca Lelew <rebecca.lelew@springernature.com>
2019-08-23 11:34:06 +0100

    Bit of tidying up, including separating Ascii Characters and drawings into separate file

---
+++ src/main/scala/AsciiCharacters.scala
@@ -0,0 +1,44 @@
+trait AsciiCharacters {
+  val person = "\uC6C3"
+  val bottomLeftJoin = "\u255a"
+  val bottomRightJoin = "\u255d"
+  val horizontalBorder = "\u2550"
+  val verticalBorder = "\u2551"
+  val topRightJoin = "\u2557"
+  val topLeftJoin = "\u2554"
+  val floor = "\u2500"
+
+  def emptySpaceOfLength(n: Int): String = " " * n
+
+  def floorSpaceOfLength(n: Int): String = floor * n
+}
+
+trait Lifts extends AsciiCharacters {
+  private val topOfLift = topLeftJoin + horizontalBorder * 6 + topRightJoin
+  private val middleOfLift = verticalBorder + " " * 6 + verticalBorder
+  private val bottomOfLift = bottomLeftJoin + horizontalBorder * 6 + bottomRightJoin
+  private val middleOfLiftWithLeftDoorOpen = " " * 7 + verticalBorder
+  private val middleOfLiftWithRightDoorOpen = verticalBorder + " " * 7
+  private val middleOfLiftWithPerson = verticalBorder + " " * 2 + person + " " * 2 + verticalBorder
+  private val middleOfLIftWithRightDoorOpenAndPerson = verticalBorder + " " * 2 + person + " " * 3
+  private val middleOfLiftWithLeftDoorOpenAndPerson = " " * 3 + person + " " * 2 + verticalBorder
+
+  val emptyLift: List[String] = lift(middleOfLift, None)
+  val liftWithLeftDoorOpen: List[String] = lift(middleOfLiftWithLeftDoorOpen, None)
+  val liftWithRightDoorOpen: List[String] = lift(middleOfLiftWithRightDoorOpen, None)
+  val liftWithPerson: List[String] = lift(middleOfLift, Some(middleOfLiftWithPerson))
+  val liftWithRightDoorOpenAndPerson: List[String] = lift(middleOfLiftWithRightDoorOpen, Some(middleOfLIftWithRightDoorOpenAndPerson))
+  val liftWithLeftDoorOpenAndPerson: List[String] = lift(middleOfLiftWithLeftDoorOpen, Some(middleOfLiftWithLeftDoorOpenAndPerson))
+
+  private def lift(middle: String, bottom: Option[String]): List[String] =
+    List(topOfLift, middle, middle, middle, middle, bottom.getOrElse(middle), bottomOfLift)
+}
+
+trait Scenery extends AsciiCharacters {
+  val emptyWaitingSpace: String = emptySpaceOfLength(18)
+  val floorWaitingSpace: String = floorSpaceOfLength(17) + emptySpaceOfLength(1)
+  val waitingSpaceWithPerson: String = emptySpaceOfLength(14) + person + emptySpaceOfLength(2)
+  val exitFloorSpace: String = emptySpaceOfLength(1) + floorSpaceOfLength(5) + emptySpaceOfLength(1)
+  val exitSpaceWithPerson: String = emptySpaceOfLength(2) + person + emptySpaceOfLength(2)
+  val emptyLiftSpace = emptySpaceOfLength(8)
+}
--- src/main/scala/Renderer.scala
+++ src/main/scala/Renderer.scala
@@ -1,23 +1,16 @@
 object Main extends App {
-  def main(printer: PrintDestination = StdOutPrinter): Unit = {
+  def main(): Unit = {
     SceneRenderer.states.foreach(s => {
-        printer.print(s.mkString("\n")); Thread.sleep(500)
+        println(s.mkString("\n")); Thread.sleep(500)
     })
   }
   main()
 }
 
-trait PrintDestination {
-  def print(string: String): Unit
-}
-
-object StdOutPrinter extends PrintDestination {
-  override def print(string: String): Unit = println(string)
-}
-
-object SceneRenderer extends Scenery with Lifts {
+object SceneRenderer extends Scenery with Lifts with AsciiCharacters {
   def convertStateToScene(eState: ElevatorState): SceneToRender = {
-    val lift = LiftToRender(eState.lifts.head.location.toInt, eState.lifts.head.people.size, eState.lifts.head.doorsOpen)
+    val firstLift = eState.lifts.head
+    val lift = LiftToRender(firstLift.location.toInt, firstLift.people.size, firstLift.doorsOpen)
     val peopleWaiting = eState.peopleWaiting.map(person => PersonOnFloor(person.start))
     val exiters = eState.exiters.map(person => PersonOnFloor(person.start))
     SceneToRender(lift, peopleWaiting, exiters)
@@ -33,36 +26,32 @@ object SceneRenderer extends Scenery with Lifts {
       case _ => emptyLift
     }
 
-    val renderedWaitingSpace = (1 to 3).toList.map(x => if (peopleWaitingOnFloor(x, scene.peopleWaiting) > 0) waitingSpaceWithPerson else emptyWaitingSpace)
-    val renderedExitSpaces = (1 to 3).toList.map(x => if (peopleOnFloorWithCompleteJourneys(x, scene.exiters) > 0) exitSpaceWithPerson else " ")
-
-    def getRowString(n: Int, lift: LiftToRender, renderedLift: List[String]): String =
-      getWaitingSpaceString(n) + liftString(lift, renderedLift, n) + exitSpaceString(n)
+    val renderedWaitingSpace = (1 to 3).toList.map(x => if (peopleOnFloor(x, scene.peopleWaiting) > 0) waitingSpaceWithPerson else emptyWaitingSpace)
+    val renderedExitSpaces = (1 to 3).toList.map(x => if (peopleOnFloor(x, scene.exiters) > 0) exitSpaceWithPerson else " ")
 
     def getWaitingSpaceString(n: Int): String = exitOrWaitingSpaceString(n, floorWaitingSpace, emptyWaitingSpace, renderedWaitingSpace)
 
     def exitSpaceString(n: Int): String = exitOrWaitingSpaceString(n, exitFloorSpace, " ", renderedExitSpaces)
 
-    def exitOrWaitingSpaceString(n: Int, floor: String, emptySpace: String, renderedPersonSpace: List[String]) =
-      n % 9 match {
-        case 0 => floor
-        case 1 => renderedPersonSpace(n / 9)
-        case _ => emptySpace
-      }
+    def getRowString(n: Int, lift: LiftToRender, renderedLift: List[String]): String =
+      getWaitingSpaceString(n) + liftString(lift, renderedLift, n) + exitSpaceString(n)
 
     (0 to 25).map(getRowString(_, scene.lift, renderedLift)).toList.reverse
   }
+
+  def exitOrWaitingSpaceString(n: Int, floor: String, emptySpace: String, renderedPersonSpace: List[String]) =
+    n % 9 match {
+      case 0 => floor
+      case 1 => renderedPersonSpace(n / 9)
+      case _ => emptySpace
+    }
 
   private def liftString(lift: LiftToRender, renderedLift: List[String], position: Int): String =
     if ((0 to 6) contains (position - lift.position)) {
       renderedLift(6 - (position - lift.position))
     } else emptyLiftSpace
 
-  private def peopleWaitingOnFloor(floor: Int, peopleWaiting: List[PersonOnFloor]): Int =
-    peopleWaiting.count(p => p.floor == floor)
-
-  private def peopleOnFloorWithCompleteJourneys(floor: Int, peopleWithCompleteJourneys: List[PersonOnFloor]): Int =
-    peopleWithCompleteJourneys.count(p => p.floor == floor)
+  private def peopleOnFloor(floor: Int, people: List[PersonOnFloor]): Int = people.count(p => p.floor == floor)
 
   val s1: List[String] = createScene(SceneToRender(LiftToRender(0, 0, ""), List(PersonOnFloor(1)), List()))
   val s2: List[String] = createScene(SceneToRender(LiftToRender(0, 0, "left"), List(PersonOnFloor(1)), List()))
@@ -81,52 +70,6 @@ object SceneRenderer extends Scenery with Lifts {
   val s15: List[String] = createScene(SceneToRender(LiftToRender(9, 0, "right"), List(), List(PersonOnFloor(2))))
   val s16: List[String] = createScene(SceneToRender(LiftToRender(9, 0, ""), List(), List(PersonOnFloor(2))))
   val states = List(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16)
-
-}
-
-trait AsciiCharacters {
-  val person = "\uC6C3"
-  val bottomLeftJoin = "\u255a"
-  val bottomRightJoin = "\u255d"
-  val horizontalBorder = "\u2550"
-  val verticalBorder = "\u2551"
-  val topRightJoin = "\u2557"
-  val topLeftJoin = "\u2554"
-  val floor = "\u2500"
-
-  def emptySpaceOfLength(n: Int): String = " " * n
-
-  def floorSpaceOfLength(n: Int): String = floor * n
-}
-
-trait Lifts extends AsciiCharacters {
-  private val topOfLift = topLeftJoin + horizontalBorder * 6 + topRightJoin
-  private val middleOfLift = verticalBorder + " " * 6 + verticalBorder
-  private val bottomOfLift = bottomLeftJoin + horizontalBorder * 6 + bottomRightJoin
-  private val middleOfLiftWithLeftDoorOpen = " " * 7 + verticalBorder
-  private val middleOfLiftWithRightDoorOpen = verticalBorder + " " * 7
-  private val middleOfLiftWithPerson = verticalBorder + " " * 2 + person + " " * 2 + verticalBorder
-  private val middleOfLIftWithRightDoorOpenAndPerson = verticalBorder + " " * 2 + person + " " * 3
-  private val middleOfLiftWithLeftDoorOpenAndPerson = " " * 3 + person + " " * 2 + verticalBorder
-
-  val emptyLift: List[String] = lift(middleOfLift, None)
-  val liftWithLeftDoorOpen: List[String] = lift(middleOfLiftWithLeftDoorOpen, None)
-  val liftWithRightDoorOpen: List[String] = lift(middleOfLiftWithRightDoorOpen, None)
-  val liftWithPerson: List[String] = lift(middleOfLift, Some(middleOfLiftWithPerson))
-  val liftWithRightDoorOpenAndPerson: List[String] = lift(middleOfLiftWithRightDoorOpen, Some(middleOfLIftWithRightDoorOpenAndPerson))
-  val liftWithLeftDoorOpenAndPerson: List[String] = lift(middleOfLiftWithLeftDoorOpen, Some(middleOfLiftWithLeftDoorOpenAndPerson))
-
-  private def lift(middle: String, bottom: Option[String]): List[String] =
-    List(topOfLift, middle, middle, middle, middle, bottom.getOrElse(middle), bottomOfLift)
-}
-
-trait Scenery extends AsciiCharacters {
-  val emptyWaitingSpace: String = emptySpaceOfLength(18)
-  val floorWaitingSpace: String = floorSpaceOfLength(17) + emptySpaceOfLength(1)
-  val waitingSpaceWithPerson: String = emptySpaceOfLength(14) + person + emptySpaceOfLength(2)
-  val exitFloorSpace: String = emptySpaceOfLength(1) + floorSpaceOfLength(5) + emptySpaceOfLength(1)
-  val exitSpaceWithPerson: String = emptySpaceOfLength(2) + person + emptySpaceOfLength(2)
-  val emptyLiftSpace = emptySpaceOfLength(8)
 }
 
 case class LiftToRender(position: Int, people: Int, doorsOpen: String)
